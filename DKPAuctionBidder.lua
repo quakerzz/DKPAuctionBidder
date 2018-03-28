90a2e1e38e80fc4124972ee8e5f555f442a25033
quakerzz <37505599+quakerzz@users.noreply.github.com>
2018-03-28 22:21:51 +0200

    fixed some stuff

--- DKPAuctionBidder.lua
+++ DKPAuctionBidder.lua
@@ -3,7 +3,11 @@ local DKPAuctionBidder_SOTAprefix = "SOTAv1"
 
 local DKPAuctionBidder_CHAT_END					= "|r"
 local DKPAuctionBidder_COLOUR_INTRO				= "|c80F0F0F0"
-local DKPAuctionBidder_COLOUR_CHAT				= "|c8040A0F8"
+local DKPAuctionBidder_COLOUR_CHAT				= "|c8040A0F8"  
+local DKPAuctionBidder_AuctionState             = 0 -- 0: No Auction, 1: Auction - no bids, 2: Auction - with bids
+local DKPAuctionBidder_PlayerDKP                = 0
+
+local currentbid = {}
 
 function DKPAuctionBidder_OnEvent(event, arg1, arg2, arg3, arg4, arg5)
     if (event == "CHAT_MSG_ADDON") then
@@ -15,6 +19,7 @@ function DKPAuctionBidder_OnLoad()
     this:RegisterEvent("ADDON_LOADED");
     this:RegisterEvent("CHAT_MSG_ADDON");
     DKPList_MinimapButtonFrame:Show()
+    DKPAuctionBidder_GetPlayerDKP()
 end
 
 function DKPAuctionBidder_BidMinOnClick()
@@ -43,6 +48,14 @@ end
 
 function DKPAuctionBidder_MinimapButtonOnClick()
     DKPAuctionBidderUIFrame:Show()
+    if DKPAuctionBidder_AuctionState == 0 then
+        getglobal("DKPAuctionBidderHighestBidTextButtonText"):SetText("Highest Bid: No Auction")
+    elseif DKPAuctionBidder_AuctionState == 1 then
+        getglobal("DKPAuctionBidderHighestBidTextButtonText"):SetText("Highest Bid: No Bids")
+    elseif DKPAuctionBidder_AuctionState == 2 then
+        getglobal("DKPAuctionBidderHighestBidTextButtonText"):SetText("Highest Bid: " ..currentbid[3] " DKP by "..currentbid[4])
+    end
+    getglobal("DKPAuctionBidderPlayerDKPButtonText"):SetText("Your DKP: " ..DKPAuctionBidder_PlayerDKP)
 end
 
 function DKPAuctionBidder_OnChatMsgAddon(event, prefix, msg, channel, sender)
@@ -53,19 +66,53 @@ function DKPAuctionBidder_OnChatMsgAddon(event, prefix, msg, channel, sender)
     local message = string.sub(msg, 1, string.len("HIGEST_BID")+1)
     local text = getglobal("DKPAuctionBidderHighestBidTextButtonText"):GetText()
 
-    local currentbid = DKPAuctionBidder_SplitString(msg)
+    currentbid = DKPAuctionBidder_SplitString(msg)
 
     if prefix == DKPAuctionBidder_SOTAprefix then
         if msg == "SOTA_AUCTION_START" then
             DKPAuctionBidderUIFrame:Show()
             getglobal("DKPAuctionBidderHighestBidTextButtonText"):SetText("Highest Bid: No Bids")
+            getglobal("DKPAuctionBidderPlayerDKPButtonText"):SetText("Your DKP: " ..DKPAuctionBidder_PlayerDKP)
+            DKPAuctionBidder_AuctionState = 1
         elseif message == "HIGHEST_BID" then
-            if currentbid[4] == UnitName("player") then currentbid[4] = currentbid[4] .."(you)"
-            getglobal("DKPAuctionBidderHighestBidTextButtonText"):SetText("Highest Bid: " ..currentbid[3] " DKP by "..currentbid[4])
+            --if currentbid[4] == UnitName("player") then currentbid[4] = currentbid[4] .."(you)"
+            getglobal("DKPAuctionBidderHighestBidTextButtonText"):SetText("Highest Bid: " ..currentbid[3] .." DKP by "..currentbid[4])
+            DKPAuctionBidder_AuctionState = 2
         elseif msg == "SOTA_AUCTION_FINISH" then
             getglobal("DKPAuctionBidderHighestBidTextButtonText"):SetText("Highest Bid: No Auction")
+            DKPAuctionBidder_AuctionState = 0
         end
     end
+end
+
+function DKPAuctionBidder_GetPlayerDKP()
+	local memberCount = GetNumGuildMembers();
+	local note
+	for n=1,memberCount,1 do
+		local name, rank, _, _, class, zone, publicnote, officernote, online = GetGuildRosterInfo(n)
+
+        if name == UnitName("player") then
+		    if not zone then
+		    	zone = "";
+		    end
+
+		    note = publicnote
+
+		    if not note or note == "" then
+		    	note = "<0>";
+		    end
+        
+		    if not online then
+		    	online = 0;
+		    end
+        
+		    local _, _, dkp = string.find(note, "<(-?%d*)>")
+		    if not dkp then
+		    	dkp = 0;
+            end
+            DKPAuctionBidder_PlayerDKP = (1*dkp)
+        end
+	end
 end
 
 function DKPAuctionBidder_SplitString(inputstr)
